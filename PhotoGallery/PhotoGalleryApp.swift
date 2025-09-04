27b645b826b89043fcbaf2b13aa8c4589a2a491e
MD ALIF HOSSAIN <45007468+Alif-bot@users.noreply.github.com>
2025-09-04 10:47:52 +0600

    Initial project setup

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Md Alif Hossain on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
